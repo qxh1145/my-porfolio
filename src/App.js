c96973bbf525720c0ab9694402819cb4a20b44c9
qxh1145 <knightdragon184@gmail.com>
2025-10-06 10:00:58 +0700

    update welcome msg

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Threads from './components/Threads';
 import ThemeToggle from './components/ThemeToggle';
 import Nav from './components/Nav';
+import LandingSplash from './components/LandingSplash';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
     <html style={{marginBottom: '50px'}}>
+    <LandingSplash duration={2000} fadeDuration={600} />
     <div style={{position: 'absolute', top: '80vh', left: '-8vh', }}><ThemeToggle /></div>
     
     <div
@@ -17,7 +19,8 @@ function App() {
         border: '1px solid var(--border-color)',
         margin: '5vh 5vh 0 5vh',
         height: '89vh',
-        // display:'block'
+        position: 'relative',
+        overflow: 'hidden',
       }}
     >
       <div style={{position: 'absolute'}}>
@@ -26,18 +29,28 @@ function App() {
       
       <div style={{position: "absolute", top: '25%', textDecoration: 'none'}}><Nav /></div>
 
-      <div style={{height: '89vh', position: 'absolute', width: '95%', display: 'block'}}> 
-      <Threads
-        amplitude={4}
-        distance={0}
-        enableMouseInteraction={true}
-        color={[1, 1, 1]}
-      />
+      <div
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+          zIndex: 0,
+        }}
+      >
+        <Threads
+          amplitude={4}
+          distance={0}
+          enableMouseInteraction={true}
+          color={[1, 1, 1]}
+          style={{ width: '100%', height: '100%' }}
+        />
       </div>
       
       <About />
 
     </div>
+    <span style={{marginLeft: '4%', color: '#D3D3D3', fontFamily: 'Courier New, Courier, monospace'}}>Inspired by Keita Yamada porfolio</span>
     </html>
     </>
   );
